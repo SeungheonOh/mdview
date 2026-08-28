aaedc0121467acf3d64d9c1e88dd44e2f9783d28
Seungheon Oh <me@sh5.org>
2026-08-28 14:49:42 -0400

    fix: generate valid Homebrew cask metadata

--- Casks/mdiew.rb
+++ Casks/mdiew.rb
@@ -1,12 +1,13 @@
 cask "mdiew" do
   arch arm: "aarch64", intel: "x86_64"
+
   version "0.1.13"
   sha256 arm:   "e3cc9e3723fd521063e67d91e37668ea1a593aa46235e46f8c0c774492d41cff",
          intel: "b4d8e96e3608f5c3068b4803b842ce0e57c390187f293b8122fac247dafe70cc"
 
   url "https://github.com/SeungheonOh/mdiew/releases/download/v#{version}/mdiew-#{arch}-apple-darwin.app.zip"
   name "mdiew"
-  desc "A fast, native macOS markdown viewer"
+  desc "Fast native Markdown viewer"
   homepage "https://github.com/SeungheonOh/mdiew"
 
   livecheck do
@@ -14,7 +15,7 @@ cask "mdiew" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "mdiew.app"
 
